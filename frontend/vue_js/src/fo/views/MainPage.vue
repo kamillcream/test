c349cf7f45e537fca59bd38cd7aefd53195265e7
DESKTOP-BRF4M8J\Paul <qkdnf325@naver.com>
2025-05-08 14:58:24 +0900

    design: 공통 컴퍼넌트 셋팅 완료

---
+++ frontend/vue_js/src/fo/views/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Freelancer_service</h1>
+    <div class="background-container">
+      <img alt="Vue logo" src="@/assets/logo.png" />
+    </div>
+    <h1>임시 메인페이지입니다.</h1>
+    <h1><router-link to="/test">공통 컴퍼넌트 바로가기</router-link></h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: #d3d3d3; /* 회색 배경 */
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
