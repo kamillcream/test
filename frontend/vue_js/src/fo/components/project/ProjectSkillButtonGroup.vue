f82793ba1ce3f3d37c446b65d3db3c93fc6407bc
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-08 11:40:26 +0900

    feat: 프로젝트 등록 페이지에 표출될 사용 기술 및 우대 기술 버튼 그룹

---
+++ frontend/vue_js/src/fo/components/project/ProjectSkillButtonGroup.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-flex gap-2 flex-wrap">
+      <div
+      v-for="skill in selectedSkills"
+      :key="skill.id"
+      class="btn btn-rounded btn-light d-flex align-items-center gap-2 mb-2 btn-3d position-relative"
+      style="padding-right: 24px;"
+      >
+        <img :src="skill.imageUrl" :alt="skill.name" width="20" height="20">
+        <span>{{skill.name}}</span>
+        <a
+          href="#"
+          @click.prevent="deleteSkill(skill.id)"
+          class="position-absolute end-0 me-2 text-grey text-decoration-none"
+          style="top: 50%; transform: translateY(-50%);"
+          title="삭제"
+        >×</a>   
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    selectedSkills: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ['remove'],
+  methods: {
+    deleteSkill(id) {
+      this.$emit('remove', id)
+    }
+  }
+}
+</script>
