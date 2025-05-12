e08277f4e26cc92a7daa94979a53fcb769696b7c
DESKTOP-BRF4M8J\Paul <qkdnf325@naver.com>
2025-05-12 17:21:37 +0900

    fix: MyPage route 수정

--- frontend/vue_js/src/fo/components/mypage/MyPageSideBar.vue
+++ frontend/vue_js/src/fo/components/mypage/MyPageSideBar.vue
@@ -5,55 +5,53 @@
       <h5 class="font-weight-semi-bold">마이 페이지</h5>
       <ul class="nav nav-list flex-column mb-5">
         <li class="nav-item">
-          <router-link class="nav-link" to="#">소속 공고 관리</router-link>
+          <span class="nav-link">소속 공고 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link" to="/affiliation/update"
+              <router-link class="nav-link" to="/mypage/affiliationEdit"
                 >소속 정보 수정</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/affiliation/members"
+              <router-link class="nav-link" to="/mypage/affiliatedMembers"
                 >소속 인원 목록</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/affiliation/applicants"
+              <router-link
+                class="nav-link"
+                to="/mypage/affiliationApplicantList"
                 >지원자 현황</router-link
               >
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="#">프로젝트 관리</router-link>
+          <span class="nav-link">프로젝트 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link active" to="/project/list"
+              <router-link class="nav-link" to="/mypage/projectListPage"
                 >프로젝트 공고 목록</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/project/applications"
-                >지원 내역</router-link
-              >
+              <router-link class="nav-link" to="#">지원 내역</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/project/scrap"
-                >스크랩 내역</router-link
-              >
+              <router-link class="nav-link" to="#">스크랩 내역</router-link>
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="#">회원 정보 관리</router-link>
+          <span class="nav-link">회원 정보 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link" to="/member/update"
+              <router-link class="nav-link" to="/mypage/informationEdit"
                 >회원 정보 수정</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/member/withdrawal"
+              <router-link class="nav-link" to="/mypage/withdraw"
                 >회원 탈퇴</router-link
               >
             </li>
@@ -67,60 +65,58 @@
       <h5 class="font-weight-semi-bold">마이 페이지</h5>
       <ul class="nav nav-list flex-column mb-5">
         <li class="nav-item">
-          <router-link class="nav-link" to="#">이력서 관리</router-link>
+          <span class="nav-link">이력서 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link active" to="/resume/register"
+              <router-link class="nav-link" to="/mypage/resumeForm"
                 >이력서 등록</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/resume/list"
+              <router-link class="nav-link" to="/mypage/resumeList"
                 >이력서 목록</router-link
               >
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="#">소속 관리</router-link>
+          <span class="nav-link">소속 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link" to="/affiliation/applications"
+              <router-link
+                class="nav-link"
+                to="/mypage/affiliatedJobApplications"
                 >지원 내역</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/affiliation/scrap"
+              <router-link class="nav-link" to="/mypage/affiliatedScrap"
                 >스크랩 내역</router-link
               >
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="#">프로젝트 관리</router-link>
+          <span class="nav-link">프로젝트 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link" to="/project/applications"
-                >지원 내역</router-link
-              >
+              <router-link class="nav-link" to="#">지원 내역</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/project/scrap"
-                >스크랩 내역</router-link
-              >
+              <router-link class="nav-link" to="#">스크랩 내역</router-link>
             </li>
           </ul>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="#">회원 정보 관리</router-link>
+          <span class="nav-link">회원 정보 관리</span>
           <ul>
             <li class="nav-item">
-              <router-link class="nav-link" to="/member/update"
+              <router-link class="nav-link" to="/mypage/informationEdit"
                 >회원 정보 수정</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/member/withdrawal"
+              <router-link class="nav-link" to="/mypage/withdraw"
                 >회원 탈퇴</router-link
               >
             </li>
@@ -138,3 +134,14 @@ const authStore = useAuthStore()
 
 //추후 로그인 기능 완성 이후 수정필요
 </script>
+
+<style scoped>
+.router-link-exact-active {
+  font-weight: bold;
+}
+
+span.nav-link {
+  color: black;
+  font-weight: bold;
+}
+</style>
