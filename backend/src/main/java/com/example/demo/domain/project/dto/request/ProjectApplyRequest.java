778825b98bc2de421fa3c8b10827de849d01afe7
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-14 17:57:04 +0900

    feat: 프로젝트 지원 요청 Dto

---
+++ backend/src/main/java/com/example/demo/domain/project/dto/request/ProjectApplyRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.project.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectApplyRequest {
+	private long resumeSq;
+	private String projectApplicationTyp; //PERSONAL or COMPANY
+}
