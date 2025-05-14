f0cda4e2d33269dec3850309e3f74f5420d295bf
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-14 17:55:14 +0900

    feat: 공통 코드 매핑 enum 및 mapper

---
+++ backend/src/main/java/com/example/demo/common/ParentCodeEnum.java
@@ -0,0 +1,18 @@
+package com.example.demo.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ParentCodeEnum {
+	
+	MEMBER_TYPE(300), 
+	APPLICATION(500), 
+	DEVELOPER_GRADE(700), 
+	CONTRACT_TYPE(900),
+	JOB_POSITION(1000),
+	EDUCATION(2100);
+	private long code;
+	
+}
