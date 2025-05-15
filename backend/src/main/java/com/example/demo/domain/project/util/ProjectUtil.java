3301830afa68164a68f6d7798ca4d3cbb894b66f
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-15 18:03:32 +0900

    feat: 프로젝트 관련 유틸리티 클래스 추가

---
+++ backend/src/main/java/com/example/demo/domain/project/util/ProjectUtil.java
@@ -0,0 +1,66 @@
+package com.example.demo.domain.project.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.common.mapper.CommonCodeMapper;
+import com.example.demo.domain.project.mapper.AddressMapper;
+import com.example.demo.domain.project.mapper.CompanyMapper;
+import com.example.demo.domain.project.mapper.ProjectMapper;
+import com.example.demo.domain.project.mapper.SkillMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProjectUtil {
+	private final AddressMapper addressMapper;
+	private final CompanyMapper companyMapper;
+	private final SkillMapper skillMapper;
+	private final CommonCodeMapper commonCodeMapper;
+	private final ProjectMapper projectMapper;
+	
+	
+	
+	public String convertAddressSqToName(Long addressSq) {
+		return addressMapper.findAddressBySq(addressSq);
+	}
+	
+	public String convertCompanySqToName(Long companySq) {
+		return companyMapper.findCompanyNmByCompanySq(companySq);
+	}
+	
+	public int calcaulateRemainingDay(LocalDate recruitEndDt) {
+		return (int) ChronoUnit.DAYS.between(LocalDate.now(), recruitEndDt);
+	}
+	
+	public List<String> fetchReqSkillsByProjectSq(Long projectSq) {
+		return skillMapper.findAllReqSkillsByProjectSq(projectSq);
+	}
+	
+	public List<String> fetchPreferSkillsByProjectSq(Long projectSq) {
+		return skillMapper.findAllPreferSkillsByProjectSq(projectSq);
+	}
+	
+	public List<String> fetchWorkTypesByProjectSq(Long projectSq) {
+		return projectMapper.findWorkTypesByProjectSq(projectSq);
+	}
+	
+	public List<String> fetchJobsByProjectSq(Long projectSq) {
+		return projectMapper.findJobsByProjectSq(projectSq);
+	}
+	
+	public Map<String, LocalDateTime> fetchInterviewTimesBySq(Long projectSq){
+		return projectMapper.findInterviewTimesBySq(projectSq);
+	}
+	
+	public String convertCommonCodeSqToNm(Long codeSq) {
+		return commonCodeMapper.findCommonCodeNmBySq(codeSq);
+	}
+}
