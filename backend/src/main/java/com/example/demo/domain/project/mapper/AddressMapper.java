525a2921d522930ec5e6144283082a843cd7334c
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-21 17:03:49 +0900

    feat: 주소 생성 쿼리 추가

--- backend/src/main/java/com/example/demo/domain/project/mapper/AddressMapper.java
+++ backend/src/main/java/com/example/demo/domain/project/mapper/AddressMapper.java
@@ -3,7 +3,12 @@ package com.example.demo.domain.project.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.demo.domain.project.dto.AddressInsertDto;
+import com.example.demo.domain.project.dto.response.AreaInfoResponse;
+
 @Mapper
 public interface AddressMapper {
 	String findAddressBySq(@Param("addressSq") Long addressSq);
+	Long createAddress(AddressInsertDto dto);
+	AreaInfoResponse findAreaInfoBySq(@Param("addressSq") Long addressSq);
 }
