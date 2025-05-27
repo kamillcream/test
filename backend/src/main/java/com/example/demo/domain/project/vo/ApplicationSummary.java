69bc8746a432e5d14de633ad34512c043cf9e1f5
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-05-27 18:00:58 +0900

    feat: 프로젝트 지원 vo 에 값 추가

--- backend/src/main/java/com/example/demo/domain/project/vo/ApplicationSummary.java
+++ backend/src/main/java/com/example/demo/domain/project/vo/ApplicationSummary.java
@@ -2,9 +2,7 @@ package com.example.demo.domain.project.vo;
 
 import java.time.LocalDateTime;
 
-import com.example.demo.domain.mypage.vo.ResumeVo;
-import com.example.demo.domain.project.entity.Project;
-import com.example.demo.domain.project.entity.ProjectApplicationEntity;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApplicationSummary {
+	private Long applicationSq;
+	private Long projectSq;
+	private Long resumeSq;
 	private String projectTitle;
 	private String companyTitle;
-	private LocalDateTime appliedDt;
-	private Long resumeSq;
 	private String resumeTitle;
-	private Integer applicantCnt;
-	private LocalDateTime readApplicationDt;
 	private String applicantType;
+	private Integer applicantCnt;
+	private LocalDateTime appliedDt;
+	private LocalDateTime readApplicationDt;
+	private boolean isRecruitEnded;
 }
