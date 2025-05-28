5f069d7aafcb19f0cc3063865beefa50ec0d8cde
Analog-PaulBaek <qkdnf325@naver.com>
2025-05-28 17:31:57 +0900

    feat : 소속정보조회 API

---
+++ backend/src/main/java/com/example/demo/domain/mypage/dto/response/AffiliationInfoResponseDTO.java
@@ -0,0 +1,53 @@
+package com.example.demo.domain.mypage.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.example.demo.domain.mypage.dto.AddressDTO;
+import com.example.demo.domain.mypage.dto.CompanyInfoDTO;
+import com.example.demo.domain.mypage.dto.UserInfoDTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AffiliationInfoResponseDTO {
+    private String companyNm;
+    private String companyCeoNm;
+    private String companyOpenDt;
+    private String companyUrl;
+    private String companyGreetingTxt;
+    private String companyIsRecruitingYn;
+    private String userPhoneNum;
+    private String zonecode;
+    private String address;
+    private String detailAddress;
+    private String sigungu;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private List<String> tagNm;
+
+    public static AffiliationInfoResponseDTO of(
+            CompanyInfoDTO company,
+            UserInfoDTO user,
+            AddressDTO address,
+            List<String> tags) {
+        return AffiliationInfoResponseDTO.builder()
+                .companyNm(company.getCompanyNm())
+                .companyCeoNm(company.getCompanyCeoNm())
+                .companyOpenDt(company.getCompanyOpenDt())
+                .companyUrl(company.getCompanyUrl())
+                .companyGreetingTxt(company.getCompanyGreetingTxt())
+                .companyIsRecruitingYn(company.getCompanyIsRecruitingYn())
+                .userPhoneNum(user.getUserPhoneNum())
+                .zonecode(address.getZonecode())
+                .address(address.getAddress())
+                .detailAddress(address.getDetailAddress())
+                .sigungu(address.getSigungu())
+                .latitude(address.getLatitude())
+                .longitude(address.getLongitude())
+                .tagNm(tags)
+                .build();
+    }
+}
