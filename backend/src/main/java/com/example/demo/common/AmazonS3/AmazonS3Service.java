b94aee406c727e386e10a3af647ff8dad139c426
Analog-PaulBaek <qkdnf325@naver.com>
2025-05-30 16:50:30 +0900

    feat : S3 테스트 예정

---
+++ backend/src/main/java/com/example/demo/common/AmazonS3/AmazonS3Service.java
@@ -0,0 +1,52 @@
+package com.example.demo.common.AWS_S3;
+
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public List<String> uploadFile(List<MultipartFile> multipartFiles) {
+        List<String> fileNameList = new ArrayList<>();
+
+        // forEach 구문을 통해 multipartFiles 리스트로 넘어온 파일들을 순차적으로 fileNameList 에 추가
+        multipartFiles.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+            }
+            fileNameList.add(fileName);
+
+        });
+
+        return fileNameList;
+    }
+
+    // 파일명을 난수화하기 위해 UUID 를 활용하여 난수를 돌린다.
+    public String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    // "."의 존재 유무만 판단
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+        }
+    }
+
+    public void deleteFile(String fileName) {
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+        System.out.println(bucket);
+    }
+}
