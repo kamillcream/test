979027d8c5490205701650419a2a8771fe6f099f
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-06-02 17:53:42 +0900

    refactor: 미사용 import 제거

--- backend/src/main/java/com/example/demo/domain/project/controller/ProjectApplicationController.java
+++ backend/src/main/java/com/example/demo/domain/project/controller/ProjectApplicationController.java
@@ -1,11 +1,12 @@
 package com.example.demo.domain.project.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.common.ApiResponse;
+
 import com.example.demo.domain.project.dto.request.ApplicationSqRequest;
 import com.example.demo.domain.project.dto.request.ApplicationStatusRequest;
 import com.example.demo.domain.project.dto.response.ApplicationStatusList;
-import com.example.demo.domain.project.dto.response.ApplicationStatusResponse;
 import com.example.demo.domain.project.dto.response.InterviewTimeInfoResponse;
-import com.example.demo.domain.project.dto.response.ProjectAppListResponse;
+
 import com.example.demo.domain.project.service.ProjectApplicationService;
 import com.example.demo.domain.project.service.ProjectService;
+
 import com.example.demo.domain.project.vo.ApplicationSummary;
-import com.example.demo.domain.project.vo.ProjectSummary;
 
 import lombok.RequiredArgsConstructor;
 
