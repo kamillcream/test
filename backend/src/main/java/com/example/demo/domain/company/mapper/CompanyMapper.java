fa0985f1aca834fad5201e6aa15ad0cba564a49b
DESKTOP-HFVSH5E\glitt <glitt1121@naver.com>
2025-06-04 17:56:59 +0900

    feat: 검색 필터 추가

--- backend/src/main/java/com/example/demo/domain/company/mapper/CompanyMapper.java
+++ backend/src/main/java/com/example/demo/domain/company/mapper/CompanyMapper.java
@@ -6,16 +6,22 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.demo.domain.company.dto.request.BaseRequest;
+import com.example.demo.domain.company.dto.request.CompanyMemberSearchRequest;
+
 @Mapper
 public interface CompanyMapper {
     String findCompanyNmByCompanySq(@Param("companySq") Long companySq);
     String findBizNumByUserSq(@Param("userSq") Long userSq);
     String findBizNumByCompanySq(@Param("companySq") Long companySq);
     
+    Long countUsersBySearch(@Param("companySq") Long companySq, @Param("request") CompanyMemberSearchRequest request);
     Long findCompanySqByUserSq(@Param("userSq") Long userSq);
     Long findUserSqByCompanySq(@Param("companySq") Long companySq);
+    Long findCompanyMemberStatus(@Param("userSq") Long userSq);
     
-    List<Long> findUserSqsByCompanySq(@Param("companySq") Long companySq);
+    
+    List<Long> findUserSqsByCompanySqAndSearch(@Param("companySq") Long companySq, @Param("request") CompanyMemberSearchRequest request);
     
     LocalDate findCompanyJoinDt(@Param("userSq") Long userSq);
     LocalDate findCompanyLeaveDt(@Param("userSq") Long userSq);
