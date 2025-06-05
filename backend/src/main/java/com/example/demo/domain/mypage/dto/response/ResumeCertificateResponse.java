0e8f5b11da08e1eddcad061cc9052c5f1cdf2031
DESKTOP-DIBR9VC\dusle <duslek11@naver.com>
2025-06-05 14:39:11 +0900

    feat : 이력서 등록 시 자격증 관련API

--- backend/src/main/java/com/example/demo/domain/mypage/dto/response/ResumeCertificateResponse.java
+++ backend/src/main/java/com/example/demo/domain/mypage/dto/response/ResumeCertificateResponse.java
@@ -4,8 +4,5 @@ import lombok.Data;
 
 @Data
 public class ResumeCertificateResponse {
-	  private Long certificationSq;         // 자격증 순번 (PK)
-	   private Long resumeSq;                // 이력서 순번 (FK)
-	   private String certificationNm;       // 자격증 이름
-	   private String certificationIssuerNm; // 발행 기관 이름.
+    private CertificateListResponse body;
 }
